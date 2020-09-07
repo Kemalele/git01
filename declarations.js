7bf80ae5bc976ce47a0a9cabc833f70343d53ea1
Kemalelee <pikapika3072@gmail.com>
2020-09-07 13:45:04 +0600

    m

---
+++ declarations.js
@@ -0,0 +1,20 @@
+let escapeStr = "`,\\,\/,\",'"
+let arr = [4, '2']
+
+const obj = {
+    undef: "",
+    num: 1,
+    bool: false,
+    undef: undefined
+}
+
+const nested = {
+    arr: [4, undefined, '2'],
+    obj: {
+        str: "",
+        num: 1,
+        bool: false
+    }
+}
+
+console.log(escapeStr, arr, obj, nested)
