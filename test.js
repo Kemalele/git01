324c23972744a633aa76e15906360e5af2e61a82
Kemalele <kemalele17@gmail.com>
2020-09-08 09:35:01 +0600

    A

--- test.js
+++ test.js
@@ -8,18 +8,22 @@ const eq = (a, b) => {
     }
 }
 
-let smalls = Number.NEGATIVE_INFINITY
-let biggie = Number.POSITIVE_INFINITY
-
-// bigger
-t(() => typeof biggie !== 'undefined')
-t(() => biggie > 1.7976931348623157e308)
-
-// smaller
-t(() => typeof smalls !== 'undefined')
-t(() => smalls < -1.7976931348623157e308)
+const words = (str) => str.split(" ")
+const sentence = (arr) => arr.join(" ")
+const yell = (str) => str.toUpperCase()
+const whisper = (str) => '*' + str.toLowerCase() + '*'
+const capitalize = (str) => str[0].toUpperCase() + str.slice(1).toLowerCase()
+    // t(({ eq }) => eq(words('a b c'), ['a', 'b', 'c']))
+    // t(({ eq }) => eq(words('Hello  world'), ['Hello', '', 'world']))
+    // t(({ eq, ctx: r }) => eq(words(`${r} ${r} ${r}`), [r, r, r]))
+    // sentence
+t(({ eq }) => eq(capitalize('str'), 'Str'))
+t(({ eq }) => eq(capitalize('qsdqsdqsd'), 'Qsdqsdqsd'))
+t(({ eq }) => eq(capitalize('STR'), 'Str'))
+t(({ eq }) => eq(capitalize('zapZAP'), 'Zapzap'))
+t(({ eq }) => eq(capitalize('zap ZAP'), 'Zap zap'))
 
 Object.freeze(tests)
 for (let i = 0; i < tests.length; i++) {
-    console.log(tests[i]())
+    console.log(tests[i]({ eq }))
 }
