aa073f6424b0b389ffc2920a198bdfe83ddbb2ff
Kemalele <kemalele17@gmail.com>
2020-09-08 09:59:31 +0600

    a

--- test.js
+++ test.js
@@ -7,23 +7,22 @@ const eq = (a, b) => {
         return false
     }
 }
-const abs = (num) => num === 0 ? 0 : num > 0 ? num : -num
-const isPositive = (a) => a > 0 ? true : false
 
-t(() => isPositive(3))
-t(() => isPositive(1998790))
-t(() => !isPositive(-1))
-t(() => !isPositive(-0.7))
-t(() => !isPositive(-787823))
-t(() => !isPositive(0))
+const max = (a, b) => a > b ? a : b
+const min = (a, b) => a < b ? a : b
 
-t(({ eq }) => eq(abs(0), 0))
-t(({ eq }) => eq(abs(-1), 1))
-t(({ eq }) => eq(abs(-13.2), 13.2))
-t(({ eq }) => eq(abs(132), 132))
+t(() => max(0, -2) === 0)
+t(() => max(-1, 10) === 10)
+t(() => max(-13.2, -222) === -13.2)
+t(() => max(132, 133) === 133)
+
+t(() => min(0, -2) === -2)
+t(() => min(-1, 10) === -1)
+t(() => min(-13.2, -222) === -222)
+t(() => min(132, 133) === 132)
 
 
 Object.freeze(tests)
 for (let i = 0; i < tests.length; i++) {
-    console.log(tests[i]({ eq }))
+    console.log(tests[i]())
 }
