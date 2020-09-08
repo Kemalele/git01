2c8f7d145a8dc42c7e981bf50eb44fa3491a2713
Kemalelee <pikapika3072@gmail.com>
2020-09-08 15:39:12 +0600

    a

--- is.js
+++ is.js
@@ -1,11 +1,11 @@
 is.num = (arg) => typeof arg === 'number'
-is.nan = (arg) => typeof arg === 'NaN'
+is.nan = (arg) => Number.isNaN(arg)
 is.str = (arg) => typeof arg === 'string'
 is.bool = (arg) => typeof arg === 'bool'
 is.undef = (arg) => typeof arg === 'undefined'
 is.def = (arg) => typeof arg !== 'undefined'
-is.arr = (arg) => typeof arg === 'array'
-is.obj = (arg) => typeof arg === 'object'
+is.arr = (arg) => Array.isArray(arg)
+is.obj = (arg) => !Array.isArray(arg) && arg !== null && typeof arg === 'object'
 is.fun = (arg) => typeof arg === 'function'
 is.truthy = (arg) => { if (arg) { return true } return false }
 is.falsy = (arg) => { if (!arg) { return false } return false }
