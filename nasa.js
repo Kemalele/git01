988f58a157ff336e8d7c8ea61065e812a271cf6a
Kemalelee <pikapika3072@gmail.com>
2020-09-10 14:57:08 +0600

    a

---
+++ nasa.js
@@ -0,0 +1,21 @@
+const nasa = (N) => {
+    let res = ''
+
+    for (let i = 1; i <= N;i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res += 'NASA'
+        } else if (i % 5 === 0) {
+            res += 'SA'
+        }  else if (i % 3 === 0) {
+            res += 'NA'
+        } else {
+            res += i.toString()
+        }
+
+        if (i != N) {
+            res += ' '
+        }
+    }
+    
+    return res
+}
--- test.js
+++ test.js
@@ -21,16 +21,35 @@ const eqArr = (a,b) => {
     return true
 }
 
+const nasa = (N) => {
+    let res = ''
 
+    for (let i = 1; i <= N;i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            res += 'NASA'
+        } else if (i % 5 === 0) {
+            res += 'SA'
+        }  else if (i % 3 === 0) {
+            res += 'NA'
+        } else {
+            res += i.toString()
+        }
 
-console.log(chunk(['a', 'b', 'c', 'd'], 2))
-console.log(chunk(['a', 'b', 'c', 'd'], 3))
+        if (i != N) {
+            res += ' '
+        }
+    }
+    
+    return res
+}
 
+console.log(nasa(15) === '1 2 NA 4 SA NA 7 8 NA SA 11 NA 13 14 NASA')
+console.log(nasa(15))
 Object.freeze(tests)
 
 for (let i = 0; i < tests.length; i++) {
     if (!tests[i]({ eq })) {
-        console.log(tests[i]())
+        console.log(tests[i]({eq}))
     } else {
         console.log('ok')
     }
